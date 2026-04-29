08f8f77157a6d437720bea30837613727f2f6ac9
f20221537-art <f20221537@goa.bits-pilani.ac.in>
2026-04-29 16:15:42 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,5 +1,4 @@
 def get_quick_data():
-    """Returns the full overlapping dataset at once."""
     return [
         {"Instrument": "Guitar", "Start": 0, "End": 30},
         {"Instrument": "Drums", "Start": 9, "End": 30},
@@ -7,13 +6,6 @@ def get_quick_data():
     ]
 
 def get_realtime_frame(current_second):
-    """
-    Simulates the model output at a specific timestamp.
-    Matches your test: 
-    0-9: Guitar
-    9-18: Guitar + Drums
-    18-30: Guitar + Drums + Voice
-    """
     active = []
     if 0 <= current_second <= 30:
         active.append("Guitar")
